ee420e96b2c201d6bb6e661cdcc157ca3f56def8
Mingox <101676015+Mingox@users.noreply.github.com>
2022-10-26 15:55:28 -0600

    Inicializacion segunda interfaz

--- Entregable1/src/main/java/com/cenfotec/entregable1/view/LoginView.java
+++ Entregable1/src/main/java/com/cenfotec/entregable1/view/LoginView.java
@@ -1,14 +1,19 @@
 package com.cenfotec.entregable1.view;
 
 import com.cenfotec.entregable1.DatabaseConection;
+import com.cenfotec.entregable1.Main;
 import com.cenfotec.entregable1.dao.DAOException;
 import com.cenfotec.entregable1.dao.UsuarioDAO;
 import com.cenfotec.entregable1.dao.mysql.MYSQLUsuarioDAO;
 import com.cenfotec.entregable1.modelos.Usuario;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.scene.control.*;
+import javafx.stage.Stage;
 
+import java.io.IOException;
 import java.sql.Connection;
 
 
@@ -45,6 +50,13 @@ public class LoginView {
             Usuario user = dao.obtener(data);
             if (userName.getText().equals(user.getUserName()) && password.getText().equals(user.getPassword())) {
                 infoLabel.setText("Ingresó correctamente");
+                Stage stage = (Stage) loginButton.getScene().getWindow();
+                stage.close();
+                FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("prueba.fxml"));
+                Scene scene = new Scene(fxmlLoader.load(), 520, 380);
+                stage.setTitle("prueba");
+                stage.setScene(scene);
+                stage.show();
             } else {
                 infoLabel.setText("Datos incorrectos");
             }
@@ -52,10 +64,9 @@ public class LoginView {
         }catch (DAOException e){
             infoLabel.setText("Datos incorrectos");
 
+        } catch (IOException e) {
+            throw new RuntimeException(e);
         }
-
-
-
 
 
     }
---
+++ Entregable1/src/main/java/com/cenfotec/entregable1/view/controllerPrueba.java
@@ -0,0 +1,5 @@
+package com.cenfotec.entregable1.view;
+
+public class controllerPrueba {
+
+}

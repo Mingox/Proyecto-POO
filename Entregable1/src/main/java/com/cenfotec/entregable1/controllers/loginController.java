929e7e3de28f8dedef90f793d53b5c3ff5821894
Mingox <101676015+Mingox@users.noreply.github.com>
2022-10-26 21:14:30 -0600

    Adicion de controller
    No estan bien implementados, no usar hasta proxima actualizacion

--- Entregable1/src/main/java/com/cenfotec/entregable1/controllers/loginController.java
+++ Entregable1/src/main/java/com/cenfotec/entregable1/controllers/loginController.java
@@ -1,4 +1,24 @@
 package com.cenfotec.entregable1.controllers;
 
-public class loginController {
+import com.cenfotec.entregable1.Main;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class loginController extends Application {
+
+    public static void main(String[] args)
+    {
+        Application.launch(args);
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("login-view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 520, 380);
+        stage.setTitle("Login");
+        stage.setScene(scene);
+        stage.show();
+    }
 }
--- Entregable1/src/main/java/com/cenfotec/entregable1/controllers/registroController.java
+++ Entregable1/src/main/java/com/cenfotec/entregable1/controllers/registroController.java
@@ -1,4 +1,22 @@
 package com.cenfotec.entregable1.controllers;
 
-public class registroController {
+import com.cenfotec.entregable1.Main;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class registroController extends Application {
+    public static void main(String[] args)
+    {
+        Application.launch(args);
+    }
+    @Override
+    public void start(Stage stage) throws Exception {
+        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("registro.fxml"));
+        Scene registro = new Scene(fxmlLoader.load(), 570, 350);
+        stage.setTitle("Registro");
+        stage.setScene(registro);
+        stage.show();
+    }
 }

76aa3673fd2c17d78488707339c4da9de17fd4ff
Gabriel <gabo24jgl@gmail.com>
2022-10-26 18:12:52 -0600

    Nuevas interfaces y carpeta icon

---
+++ Entregable1/src/main/java/com/cenfotec/entregable1/view/ProyectoView.java
@@ -0,0 +1,63 @@
+package com.cenfotec.entregable1.view;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+
+public class ProyectoView {
+
+    @FXML
+    private TableColumn<?, ?> Categoria_P;
+
+    @FXML
+    private TableColumn<?, ?> FecCrea_P;
+
+    @FXML
+    private TableColumn<?, ?> Inv_P;
+
+    @FXML
+    private TableColumn<?, ?> Media_P;
+
+    @FXML
+    private TableColumn<?, ?> Nombre_P;
+
+    @FXML
+    private TableColumn<?, ?> UltMod_P;
+
+    @FXML
+    private Button btn01Proj;
+
+    @FXML
+    private Button btn02Proj;
+
+    @FXML
+    private Button btn03Proj;
+
+    @FXML
+    private Button btn04Proj;
+
+    @FXML
+    private Button btnNewP;
+
+    @FXML
+    private TableView<?> tabInveP;
+
+    @FXML
+    private TableView<?> tabMediaP;
+
+    @FXML
+    private TableView<?> tabProyecto;
+
+    @FXML
+    void click(ActionEvent event) {
+
+    }
+
+    @FXML
+    void handleButtonClick(ActionEvent event) {
+
+    }
+
+}
---
+++ Entregable1/src/main/java/com/cenfotec/entregable1/view/nuevoProyectoView.java
@@ -0,0 +1,54 @@
+package com.cenfotec.entregable1.view;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.CheckMenuItem;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.MenuBar;
+import javafx.scene.control.TextField;
+
+public class nuevoProyectoView {
+
+    @FXML
+    private MenuBar Cat_Np;
+
+    @FXML
+    private CheckMenuItem Nv_Np;
+
+    @FXML
+    private CheckMenuItem accionT_Np;
+
+    @FXML
+    private Button btnCrearP;
+
+    @FXML
+    private DatePicker creacion_Np;
+
+    @FXML
+    private CheckMenuItem dae_Np;
+
+    @FXML
+    private CheckMenuItem ic_Np;
+
+    @FXML
+    private CheckMenuItem jdd_Np;
+
+    @FXML
+    private CheckMenuItem pea_Np;
+
+    @FXML
+    private TextField txtNom_Np;
+
+    @FXML
+    private DatePicker ultmodif_Np;
+
+    @FXML
+    private CheckMenuItem vgd_Np;
+
+    @FXML
+    void click(ActionEvent event) {
+
+    }
+
+}
